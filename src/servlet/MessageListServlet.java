0dee23772c6b13f39b36baa1b863e0d741e7d7af
Ilya_Kortchenko <iko2607@gmail.com>
2020-05-22 18:48:37 +0300

    laba_8

---
+++ src/servlet/MessageListServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import entity.ChatMessage;
+
+@WebServlet(name = "MessageListServlet")
+public class MessageListServlet extends ChatServlet {
+    private static final long serialVersionUID = 1L;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("utf8");
+        PrintWriter pw = response.getWriter();
+        pw.println("<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><meta http-equiv='refresh' content='10'></head>");
+        pw.println("<body>");
+        for (int i=messages.size()-1; i>=0; i--) {
+            ChatMessage aMessage = messages.get(i);
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+            Date date = new Date(aMessage.getTimestamp());
+            pw.println("<div><strong>" + aMessage.getAuthor().getName() + "(" + sdf.format(date) + ")"+"</strong>: " + aMessage.getMessage() + "</div>");
+        }
+        pw.println("</body></html>");
+    }
+}
